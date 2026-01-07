047616562187717d7c4d658385d9ef91e2f930dd
Ram kumar <ramkumar724872@gmail.com>
2026-01-07 10:12:36 +0530

    Time: 2 ms (10.17%) | Memory: 49.9 MB (5.30%) - LeetSync

---
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,71 @@
+// class Solution {
+//     public int maxProfit(int[] prices) {
+//         int maxProfit=0;
+//         int bestBuy=prices[0];
+//         for(int i =1;i<prices.length;i++){
+//             if(prices[i]>bestBuy){
+//                 maxProfit=Math.max(maxProfit,prices[i]-bestBuy);
+//             }
+//             bestBuy=Math.min(bestBuy,prices[i]);
+//         }
+//         return maxProfit;
+        
+//     }
+// }
+
+// class Solution {
+//     public int maxProfit(int[] prices) {
+//         return solve(0, prices, true); // true = canBuy
+//     }
+
+//     public int solve(int index, int[] prices, boolean canBuy) {
+
+        
+//         if (index == prices.length) {
+//             return 0;
+//         }
+
+//         int skip = solve(index + 1, prices, canBuy);
+
+//         if (canBuy) {
+            
+//             int buy = -prices[index] + solve(index + 1, prices, false);
+//             return Math.max(buy, skip);
+//         } else {
+        
+//             int sell = prices[index] + solve(index + 1, prices, true);
+//             return Math.max(sell, skip);
+//         }
+//     }
+// }
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<2;j++){
+                dp[i][j]=-1;
+            }
+        }
+        return solve(0,1,prices,dp);
+
+    }
+    public int solve(int i, int flag,int[] prices,int[][] dp){
+        int n = prices.length;
+        if(i>=n) return 0;
+        if(dp[i][flag]!=-1) return dp[i][flag];
+        int Profit=0;
+        if(flag==1){
+            int buy=-prices[i] + solve(i+1,0,prices,dp);
+            int skip = 0 + solve(i+1,1,prices,dp);
+            Profit= Math.max(buy,skip);
+        }else{
+            int sell=prices[i] + solve(i+1,1,prices,dp);
+            int skip2 = 0+solve(i+1,0,prices,dp);
+            Profit=Math.max(sell,skip2);
+        }
+        dp[i][flag] = Profit;
+        return dp[i][flag];
+    }
+}
