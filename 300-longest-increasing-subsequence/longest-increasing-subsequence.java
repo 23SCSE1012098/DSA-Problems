484c8e0f870938dbde208653bfae5d04b66729c9
Ram kumar <ramkumar724872@gmail.com>
2026-01-12 09:51:52 +0530

    Time: 3 ms (95.88%) | Memory: 46.1 MB (73.79%) - LeetSync

---
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+
+        int[] tails = new int[nums.length];
+        int size = 0;
+
+        for (int num : nums) {
+            int left = 0, right = size;
+
+            // Binary search for the insertion point
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+                if (tails[mid] < num) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            }
+
+            tails[left] = num;
+            if (left == size) size++;
+        }
+
+        return size;
+    }
+}
