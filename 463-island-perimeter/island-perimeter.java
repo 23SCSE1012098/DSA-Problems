28b9bbbdc7a05f709d72cc8c46b9e8fa0f1d9446
Ram kumar <ramkumar724872@gmail.com>
2026-02-05 12:24:55 +0530

    Time: 8 ms (18.34%) | Memory: 47.2 MB (59.10%) - LeetSync

--- 463-island-perimeter/island-perimeter.java
+++ 463-island-perimeter/island-perimeter.java
@@ -1,33 +1,83 @@
+// class Solution {
+//     public int islandPerimeter(int[][] grid) {
+
+//         if (grid == null || grid.length == 0 || grid[0].length == 0) {
+//             return 0;
+//         }
+
+//         int perimeter = 0;
+//         int rows = grid.length;
+//         int cols = grid[0].length;
+
+//         for (int i = 0; i < rows; i++) {
+//             for (int j = 0; j < cols; j++) {
+
+//                 if (grid[i][j] == 1) {
+//                     perimeter += 4;
+
+//                     // shared edge with top
+//                     if (i > 0 && grid[i - 1][j] == 1) {
+//                         perimeter -= 2;
+//                     }
+
+//                     // shared edge with left
+//                     if (j > 0 && grid[i][j - 1] == 1) {
+//                         perimeter -= 2;
+//                     }
+//                 }
+//             }
+//         }
+
+//         return perimeter;
+//     }
+// }
+
 class Solution {
+
+    int perimeter = 0;
+
     public int islandPerimeter(int[][] grid) {
 
         if (grid == null || grid.length == 0 || grid[0].length == 0) {
             return 0;
         }
 
-        int perimeter = 0;
         int rows = grid.length;
         int cols = grid[0].length;
 
+        boolean[][] visited = new boolean[rows][cols];
+
+        // find first land cell
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-
                 if (grid[i][j] == 1) {
-                    perimeter += 4;
-
-                    // shared edge with top
-                    if (i > 0 && grid[i - 1][j] == 1) {
-                        perimeter -= 2;
-                    }
-
-                    // shared edge with left
-                    if (j > 0 && grid[i][j - 1] == 1) {
-                        perimeter -= 2;
-                    }
+                    dfs(grid, visited, i, j);
+                    return perimeter;
                 }
             }
         }
 
-        return perimeter;
+        return 0;
+    }
+
+    private void dfs(int[][] grid, boolean[][] visited, int i, int j) {
+
+        if (i < 0 || i >= grid.length ||
+            j < 0 || j >= grid[0].length ||
+            grid[i][j] == 0) {
+            perimeter++;   // boundary or water adds to perimeter
+            return;
+        }
+
+        if (visited[i][j]) {
+            return;
+        }
+
+        visited[i][j] = true;
+
+        dfs(grid, visited, i + 1, j);
+        dfs(grid, visited, i - 1, j);
+        dfs(grid, visited, i, j + 1);
+        dfs(grid, visited, i, j - 1);
     }
 }
