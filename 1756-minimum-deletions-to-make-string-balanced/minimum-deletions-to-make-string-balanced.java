8a9fa27fb650893b369e45250fca122bb7c813f1
Ram kumar <ramkumar724872@gmail.com>
2026-02-07 11:27:20 +0530

    Time: 21 ms (68.91%) | Memory: 47.7 MB (75.49%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
@@ -1,22 +1,40 @@
-class Solution {
-    public int minimumDeletions(String s) {
-        int count=0;
-        int n = s.length();
-        Stack<Character> stack =new Stack<>();
-        for(int i=0;i<n;i++){
-            if(!stack.empty() && s.charAt(i)=='a' && stack.peek()=='b'){
-                stack.pop();
-                count++;
-            }
+// class Solution {
+//     public int minimumDeletions(String s) {
+//         int count=0;
+//         int n = s.length();
+//         Stack<Character> stack =new Stack<>();
+//         for(int i=0;i<n;i++){
+//             if(!stack.empty() && s.charAt(i)=='a' && stack.peek()=='b'){
+//                 stack.pop();
+//                 count++;
+//             }
             
-            else{
-                stack.push(s.charAt(i));
-            }
+//             else{
+//                 stack.push(s.charAt(i));
+//             }
             
-        }
-        return count;
+//         }
+//         return count;
 
         
         
+//     }
+// }
+
+class Solution {
+    public int minimumDeletions(String s) {
+        int count=0;
+        int bcount=0;
+        int n = s.length();
+        for(int i=0;i<n;i++){
+            if(s.charAt(i)=='a'){
+                count=Math.min(bcount,count+1);
+            }
+            else{
+                bcount++;
+            }
+        }
+        return count;
+ 
     }
 }
