959f9820ad657fc6f704d35f0d8636a93ae669d1
Ram kumar <ramkumar724872@gmail.com>
2026-02-14 13:40:54 +0530

    Time: 7 ms (9.30%) | Memory: 47.1 MB (89.54%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,97 +1,103 @@
-// class Solution {
-//     public boolean isBipartite(int[][] graph) {
-//         int n = graph.length;
-//         int m = graph[0].length;
-//         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-//         int [] col = new int[n];
-//         for(int i =0;i<n;i++){
-//             col[i]=-1;
-//         }
-//         for(int i =0;i<n;i++){
-//             adj.add(new ArrayList<>());
-//         }
-//         for(int [] x : graph){
-//             for(int j=0;j<x.length;j++){
-//                 int u = x[j];
-//                 int v = x[j+1];
-//                 adj.get(u).add(v);
-//             }
-            
-//         }
-//         for(int i =0;i<n;i++){
-//             if(col[i]==-1){
-//                 if(bfs(adj,i,col)){
-//                     return true;
-//                 }
-//             }
-//         }
-//         return false;
-        
-
-        
-//     }
-//     public boolean bfs(ArrayList<ArrayList<Integer>> adj , int curr, int [] col){
-//         Queue<Integer> qu = new LinkedList<>();
-//         qu.add(curr);
-//         col[curr]=0;
-//         while(!qu.isEmpty()){
-//             curr = qu.remove();
-//             for(int i =0;i<adj[curr].size();i++){
-//                 qu.remove(curr);
-//                 if(col[i]!=0 || col[i]!=1){
-//                     col[i]=1;
-//                     if(col[curr]==col[i]){
-//                         return false;
-//                     }
-//                 }
-//             }
-//         }    
-//     }
-// }
-
-
-// USING ADJACENCY MATRIX
-
 class Solution {
     public boolean isBipartite(int[][] graph) {
-
         int n = graph.length;
-        int[] col = new int[n];
-
-        Arrays.fill(col, -1);
+        int m = graph[0].length;
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        int [] col = new int[n];
+        for(int i =0;i<n;i++){
+            col[i]=-1;
+        }
+        for(int i = 0; i < n; i++){
+            adj.add(new ArrayList<>());
+        }
 
         for(int i = 0; i < n; i++){
-            if(col[i] == -1){
-                if(!bfs(graph, i, col)){
+            for(int neighbor : graph[i]){
+                adj.get(i).add(neighbor);
+            }
+        }
+
+        for(int i =0;i<n;i++){
+            if(col[i]==-1){
+                if(!bfs(adj,i,col,1)){
                     return false;
                 }
             }
         }
-
         return true;
+        
+
+        
     }
+    public boolean bfs(ArrayList<ArrayList<Integer>> adj, int curr, int[] col, int currcol){
 
-    public boolean bfs(int[][] graph, int start, int[] col){
+        Queue<Integer> qu = new LinkedList<>();
+        qu.add(curr);
+        col[curr] = currcol;
 
-        Queue<Integer> q = new LinkedList<>();
-        q.add(start);
-        col[start] = 0;
+        while(!qu.isEmpty()){
 
-        while(!q.isEmpty()){
-            int node = q.poll();
+            int node = qu.remove();
 
-            for(int neighbor : graph[node]){
+            for(int i = 0; i < adj.get(node).size(); i++){
+
+                int neighbor = adj.get(node).get(i);  
 
                 if(col[neighbor] == -1){
-                    col[neighbor] = 1 - col[node];
-                    q.add(neighbor);
+                    col[neighbor] = 1 - col[node];     
+                    qu.add(neighbor);
                 }
                 else if(col[neighbor] == col[node]){
-                    return false;
+                    return false;                      
                 }
             }
         }
 
-        return true;
+        return true;  
     }
 }
+// USING ADJACENCY MATRIX
+
+// class Solution {
+//     public boolean isBipartite(int[][] graph) {
+
+//         int n = graph.length;
+//         int[] col = new int[n];
+
+//         Arrays.fill(col, -1);
+
+//         for(int i = 0; i < n; i++){
+//             if(col[i] == -1){
+//                 if(!bfs(graph, i, col)){
+//                     return false;
+//                 }
+//             }
+//         }
+
+//         return true;
+//     }
+
+//     public boolean bfs(int[][] graph, int start, int[] col){
+
+//         Queue<Integer> q = new LinkedList<>();
+//         q.add(start);
+//         col[start] = 0;
+
+//         while(!q.isEmpty()){
+//             int node = q.poll();
+
+//             for(int neighbor : graph[node]){
+
+//                 if(col[neighbor] == -1){
+//                     col[neighbor] = 1 - col[node];
+//                     q.add(neighbor);
+//                 }
+//                 else if(col[neighbor] == col[node]){
+//                     return false;
+//                 }
+//             }
+//         }
+
+//         return true;
+//     }
+// }
