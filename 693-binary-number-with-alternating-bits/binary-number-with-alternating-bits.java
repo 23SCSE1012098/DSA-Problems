098a96e3dcb5e0ecfc771e26a40d0422048292e7
Ram kumar <ramkumar724872@gmail.com>
2026-02-18 14:33:37 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (16.11%) - LeetSync

--- 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.java
+++ 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.java
@@ -4,11 +4,12 @@ class Solution {
             return true;
         }
         String s1= Integer.toBinaryString(n);
-        for(int i=1;i<s1.length();i++){
-            if(s1.charAt(i)==s1.charAt(i-1)){
+        int l = s1.length()-1;
+        while(l!=0){
+            if(s1.charAt(l)==s1.charAt(l-1)){
                 return false;
             }
-            
+            l--;
         }
         return true;
         
