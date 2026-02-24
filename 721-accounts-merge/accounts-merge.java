36319d8aaf197061a86a7424ccc6293b36cfc153
Ram kumar <ramkumar724872@gmail.com>
2026-02-24 12:36:21 +0530

    Time: 29 ms (82.27%) | Memory: 49.6 MB (79.75%) - LeetSync

--- 721-accounts-merge/accounts-merge.java
+++ 721-accounts-merge/accounts-merge.java
@@ -1,4 +1,4 @@
-// class Solution {
+   // class Solution {
 //     int [] parent;
 //     int [] size ;
 //     public List<List<String>> accountsMerge(List<List<String>> accounts) {
@@ -42,56 +42,138 @@
 
 /// correct code
 
+// import java.util.*;
+
+// // Disjoint Set Union (DSU) for connecting accounts
+// class DisjointSet {
+//     int[] parent;
+//     public DisjointSet(int n) {
+//         parent = new int[n];
+//         for (int i = 0; i < n; i++) parent[i] = i;
+//     }
+//     public int findUPar(int node) {
+//         if (node == parent[node]) return node;
+//         return parent[node] = findUPar(parent[node]); // Path compression
+//     }
+//     public void unionBySize(int u, int v) {
+//         int rootU = findUPar(u), rootV = findUPar(v);
+//         if (rootU != rootV) parent[rootU] = rootV;
+//     }
+// }
+
+// class Solution {
+//     public List<List<String>> accountsMerge(List<List<String>> accounts) {
+//         int n = accounts.size();
+//         DisjointSet ds = new DisjointSet(n);
+//         Map<String, Integer> mapMailNode = new HashMap<>();
+
+//         // 1. Connect accounts with common emails
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 1; j < accounts.get(i).size(); j++) {
+//                 String mail = accounts.get(i).get(j);
+//                 if (mapMailNode.containsKey(mail)) ds.unionBySize(i, mapMailNode.get(mail));
+//                 else mapMailNode.put(mail, i);
+//             }
+//         }
+
+//         // 2. Group emails by root parent
+//         Map<Integer, List<String>> merged = new HashMap<>();
+//         for (Map.Entry<String, Integer> entry : mapMailNode.entrySet()) {
+//             int root = ds.findUPar(entry.getValue());
+//             merged.computeIfAbsent(root, k -> new ArrayList<>()).add(entry.getKey());
+//         }
+
+//         // 3. Format result
+//         List<List<String>> ans = new ArrayList<>();
+//         for (Map.Entry<Integer, List<String>> entry : merged.entrySet()) {
+//             List<String> emails = entry.getValue();
+//             Collections.sort(emails);
+//             String name = accounts.get(entry.getKey()).get(0);
+//             emails.add(0, name);
+//             ans.add(emails);
+//         }
+//         return ans;
+//     }
+// }
+
+
+/// Another Solution 
+
+
 import java.util.*;
 
-// Disjoint Set Union (DSU) for connecting accounts
-class DisjointSet {
-    int[] parent;
-    public DisjointSet(int n) {
-        parent = new int[n];
-        for (int i = 0; i < n; i++) parent[i] = i;
-    }
-    public int findUPar(int node) {
-        if (node == parent[node]) return node;
-        return parent[node] = findUPar(parent[node]); // Path compression
-    }
-    public void unionBySize(int u, int v) {
-        int rootU = findUPar(u), rootV = findUPar(v);
-        if (rootU != rootV) parent[rootU] = rootV;
-    }
-}
-
 class Solution {
+    
+    int[] parent;
+    int[] size;
+    
     public List<List<String>> accountsMerge(List<List<String>> accounts) {
         int n = accounts.size();
-        DisjointSet ds = new DisjointSet(n);
-        Map<String, Integer> mapMailNode = new HashMap<>();
-
-        // 1. Connect accounts with common emails
+        
+        parent = new int[n];
+        size = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+        
+        HashMap<String, Integer> emailToIndex = new HashMap<>();
+        
+        // Step 1: Union accounts with common emails
         for (int i = 0; i < n; i++) {
             for (int j = 1; j < accounts.get(i).size(); j++) {
-                String mail = accounts.get(i).get(j);
-                if (mapMailNode.containsKey(mail)) ds.unionBySize(i, mapMailNode.get(mail));
-                else mapMailNode.put(mail, i);
+                String email = accounts.get(i).get(j);
+                
+                if (!emailToIndex.containsKey(email)) {
+                    emailToIndex.put(email, i);
+                } else {
+                    union(i, emailToIndex.get(email));
+                }
             }
         }
-
-        // 2. Group emails by root parent
-        Map<Integer, List<String>> merged = new HashMap<>();
-        for (Map.Entry<String, Integer> entry : mapMailNode.entrySet()) {
-            int root = ds.findUPar(entry.getValue());
-            merged.computeIfAbsent(root, k -> new ArrayList<>()).add(entry.getKey());
+        
+        // Step 2: Group emails by parent
+        HashMap<Integer, TreeSet<String>> merged = new HashMap<>();
+        
+        for (String email : emailToIndex.keySet()) {
+            int parentIndex = find(emailToIndex.get(email));
+            merged.putIfAbsent(parentIndex, new TreeSet<>());
+            merged.get(parentIndex).add(email);
         }
-
-        // 3. Format result
-        List<List<String>> ans = new ArrayList<>();
-        for (Map.Entry<Integer, List<String>> entry : merged.entrySet()) {
-            List<String> emails = entry.getValue();
-            Collections.sort(emails);
-            String name = accounts.get(entry.getKey()).get(0);
-            emails.add(0, name);
-            ans.add(emails);
+        
+        // Step 3: Build result
+        List<List<String>> result = new ArrayList<>();
+        
+        for (int index : merged.keySet()) {
+            List<String> list = new ArrayList<>();
+            list.add(accounts.get(index).get(0)); // name
+            list.addAll(merged.get(index));
+            result.add(list);
         }
-        return ans;
+        
+        return result;
+    }
+    
+    private int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]); // Path compression
+        }
+        return parent[x];
+    }
+    
+    private void union(int x, int y) {
+        int px = find(x);
+        int py = find(y);
+        
+        if (px == py) return;
+        
+        if (size[px] > size[py]) {
+            parent[py] = px;
+            size[px] += size[py];
+        } else {
+            parent[px] = py;
+            size[py] += size[px];
+        }
     }
 }
