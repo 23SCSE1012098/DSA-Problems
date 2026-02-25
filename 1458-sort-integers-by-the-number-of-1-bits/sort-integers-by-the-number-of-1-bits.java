88f5b1bf64bd111f02e8eac2a288bc500304cf4d
Ram kumar <ramkumar724872@gmail.com>
2026-02-25 19:26:05 +0530

    Time: 10 ms (39.84%) | Memory: 47 MB (27.94%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.java
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.java
@@ -1,23 +1,41 @@
+// class Solution {
+//     public int[] sortByBits(int[] arr) {
+//         Integer temp[] = new Integer[arr.length];
+        
+//         for(int i = 0; i < arr.length; i++){
+//             temp[i] = arr[i];
+//         }
+
+//         Arrays.sort(temp, (a, b) -> {
+//             int bitofa = Integer.bitCount(a);
+//             int bitofb = Integer.bitCount(b);
+
+//             if(bitofa == bitofb) return a - b;
+//             return bitofa - bitofb;
+//         });
+
+//         for(int i = 0; i < arr.length; i++){
+//             arr[i] = temp[i];
+//         }
+
+//         return arr;
+//     }
+// }
+
 class Solution {
     public int[] sortByBits(int[] arr) {
-        Integer temp[] = new Integer[arr.length];
+        int temp[] = new int [arr.length];
+        Arrays.sort(arr);
         
-        for(int i = 0; i < arr.length; i++){
-            temp[i] = arr[i];
+        int index=0;
+        for(int i=0;i<32;i++){
+            for(int j=0;j<arr.length;j++){
+                if(i==Integer.bitCount(arr[j])){
+                    temp[index++]=arr[j];
+                }
+            }
         }
 
-        Arrays.sort(temp, (a, b) -> {
-            int bitofa = Integer.bitCount(a);
-            int bitofb = Integer.bitCount(b);
-
-            if(bitofa == bitofb) return a - b;
-            return bitofa - bitofb;
-        });
-
-        for(int i = 0; i < arr.length; i++){
-            arr[i] = temp[i];
-        }
-
-        return arr;
+        return temp;
     }
 }
