3a52d26a758fc53e34626481f6d8b3c83e643cda
Ram kumar <ramkumar724872@gmail.com>
2026-03-27 16:26:09 +0530

    Time: 1 ms (99.47%) | Memory: 47 MB (64.00%) - LeetSync

--- 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.java
+++ 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.java
@@ -1,35 +1,63 @@
+// class Solution {
+//     public boolean areSimilar(int[][] mat, int k) {
+        
+//         int m = mat.length;
+//         int n = mat[0].length;
+//         int [][] ans = new int [m][n];
+//         for(int i=0;i<m;i++){
+//             for(int j=0;j<n;j++){
+//                 ans[i][j]=mat[i][j];
+//             }
+//         }
+//         k = k%n;
+//         for(int i=0;i<m;i++){
+//             for(int j=0;j<n;j++){
+//                 if(i%2==0){
+//                     mat[i][j]=ans[i][(j+k)%n]; //left shift
+                    
+//                 }
+//                 else{
+//                     mat[i][j] = ans[i][(j - k + n) % n]; // right shift
+//                 }
+//             }
+//         }
+//         for(int i=0;i<m;i++){
+//             for(int j =0;j<n;j++){
+//                 if(mat[i][j]!=ans[i][j]){
+//                     return false;
+//                 }
+//             }
+//         }
+//         return true;
+//     }
+
+// }
+
+
 class Solution {
     public boolean areSimilar(int[][] mat, int k) {
         
         int m = mat.length;
         int n = mat[0].length;
-        int [][] ans = new int [m][n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                ans[i][j]=mat[i][j];
-            }
-        }
         k = k%n;
+        if(k==0) return true;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-
-            
+                int curr =j;
+                int futureIdx;
                 if(i%2==0){
-                    mat[i][j]=ans[i][(j+k)%n];
+                    futureIdx=(j+k)%n; //left shift
                     
                 }
                 else{
-                    mat[i][j] = ans[i][(j - k + n) % n];
+                    futureIdx=(j - k + n) % n; // right shift
                 }
-            }
-        }
-        for(int i=0;i<m;i++){
-            for(int j =0;j<n;j++){
-                if(mat[i][j]!=ans[i][j]){
+                if(mat[i][curr]!=mat[i][futureIdx]){
                     return false;
                 }
             }
         }
+
         return true;
     }
 
