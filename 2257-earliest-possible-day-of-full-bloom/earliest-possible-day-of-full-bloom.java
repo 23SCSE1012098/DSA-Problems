9b687e7bee6006763b24764b470d92322f0906df
Ram kumar <ramkumar724872@gmail.com>
2026-04-08 07:40:45 +0530

    Time: 56 ms (47.67%) | Memory: 117.7 MB (91.71%) - LeetSync

--- 2257-earliest-possible-day-of-full-bloom/earliest-possible-day-of-full-bloom.java
+++ 2257-earliest-possible-day-of-full-bloom/earliest-possible-day-of-full-bloom.java
@@ -1,34 +1,21 @@
-import java.util.*;
 
 class Solution {
     public int earliestFullBloom(int[] plantTime, int[] growTime) {
         int n = plantTime.length;
-        
-        // pair array: {plantTime[i], growTime[i]}
-        int[][] vec = new int[n][2];
-        
-        for (int i = 0; i < n; i++) {
-            vec[i][0] = plantTime[i];
-            vec[i][1] = growTime[i];
+        int [][] dp = new int [n][2];
+        for(int i=0;i<n;i++){
+            dp[i][0]=plantTime[i];
+            dp[i][1]=growTime[i];
         }
-        
-        // sort by growTime in descending order
-        Arrays.sort(vec, (a, b) -> b[1] - a[1]);
-        
-        int prevPlantDays = 0;
-        int maxBloomDays = 0;
-        
-        for (int i = 0; i < n; i++) {
-            int currPlantTime = vec[i][0];
-            int currGrowTime = vec[i][1];
-            
-            prevPlantDays += currPlantTime;
-            
-            int currPlantBloomTime = prevPlantDays + currGrowTime;
-            
-            maxBloomDays = Math.max(maxBloomDays, currPlantBloomTime);
+        Arrays.sort(dp, (a,b) -> b[1] - a[1]);
+        int plantingday=0 , Maxtotalday=0;
+        for(int i=0;i<n;i++){
+            int ptime = dp[i][0];
+            int gtime = dp[i][1];
+
+            Maxtotalday = Math.max(Maxtotalday,plantingday+ptime+gtime);
+            plantingday = plantingday+ptime;
         }
-        
-        return maxBloomDays;
+        return Maxtotalday;
     }
 }
