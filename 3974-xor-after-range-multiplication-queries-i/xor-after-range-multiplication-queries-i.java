48891636b05c16c49d9572fc28c703172a863a5a
Ram kumar <ramkumar724872@gmail.com>
2026-04-08 14:24:33 +0530

    Time: 59 ms (88.00%) | Memory: 47.3 MB (98.67%) - LeetSync

--- 3974-xor-after-range-multiplication-queries-i/xor-after-range-multiplication-queries-i.java
+++ 3974-xor-after-range-multiplication-queries-i/xor-after-range-multiplication-queries-i.java
@@ -2,13 +2,12 @@ class Solution {
     
     public int xorAfterQueries(int[] nums, int[][] queries) {
         int mod = 1000000000+7;
-        int n = queries.length;
 
-        for(int [] query : queries){
-            int start = query[0];
-            int end = query[1];
-            int incre = query[2];
-            int multi = query[3];
+        for(int k=0;k < queries.length;k++){
+            int start = queries[k][0];
+            int end = queries[k][1];
+            int incre = queries[k][2];
+            int multi = queries[k][3];
             for(int i=start; i<=end;i+=incre){
                 nums[i]=(int)((1L * nums[i]*multi)%mod);
             }
