23b256d932792caf0372acc531eb10f492253152
Ram kumar <ramkumar724872@gmail.com>
2026-04-28 10:26:21 +0530

    Time: 3 ms (56.32%) | Memory: 47.1 MB (58.68%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,18 +1,41 @@
-import java.util.HashMap;
+// import java.util.HashMap;
 
-public class Solution {
-    int [] result = new int [2];
-    public  int[] twoSum(int[] nums, int target) {
-       for(int i= 0; i<nums.length; i++){
-        for(int j= i+1; j<nums.length; j++){
-            if(nums[i] + nums[j] == target){
-                result [0] = i;
-                result [1] = j;
-                break;
+// public class Solution {
+//     int [] result = new int [2];
+//     public  int[] twoSum(int[] nums, int target) {
+//        for(int i= 0; i<nums.length; i++){
+//         for(int j= i+1; j<nums.length; j++){
+//             if(nums[i] + nums[j] == target){
+//                 result [0] = i;
+//                 result [1] = j;
+//                 break;
+//             }
+//         }
+//        }
+//        return result;
+//     }
+
+// }
+
+
+import java.util.*;
+
+class Solution {
+    public int[] twoSum(int[] arr, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < arr.length; i++){
+            int complement = target - arr[i];
+
+            if(map.containsKey(complement)){
+                return new int[]{map.get(complement), i};
             }
-        }
-       }
-       return result;
-    }
 
+            map.put(arr[i], i);
+        }
+
+        return new int[]{-1, -1};
+    }
 }
+
+
