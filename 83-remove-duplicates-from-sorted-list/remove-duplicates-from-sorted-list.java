504fe515906d94716f2734eb14c68f2bd7a7c270
Ram kumar <ramkumar724872@gmail.com>
2026-05-06 10:59:39 +0530

    Time: 1 ms (4.71%) | Memory: 46.5 MB (6.31%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.java
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.java
@@ -8,43 +8,45 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head == null || head.next==null){
+            return head;
+        }
+        ListNode dummy = new ListNode (head.val);
+        ListNode ans = dummy;
+        ListNode curr = head.next;
+        while(curr != null){
+            if(curr.val != dummy.val){
+                ListNode temp = new ListNode (curr.val);
+
+                dummy.next = temp;
+                dummy = dummy.next;
+            }
+            else{
+                curr= curr.next;
+
+            }
+            
+        }
+        return ans;
+        
+    }
+}
+
 // class Solution {
 //     public ListNode deleteDuplicates(ListNode head) {
-//         if(head == null || head.next==null){
-//             return head;
-//         }
-//         ListNode dummy = new ListNode (head.val);
-//         ListNode ans = dummy;
-//         ListNode curr = dummy.next;
-//         while(curr != null){
-//             if(curr.val == dummy.val){
-//                 dummy.next = curr.next;
+//         ListNode curr = head;
+//         while(curr !=null && curr.next != null){
+//             if(curr.val==curr.next.val){
+//                 curr.next=curr.next.next;
 //             }
 //             else{
-//                 dummy=dummy.next;
-//                 curr= curr.next;
+//                 curr = curr.next;
 
 //             }
             
 //         }
-//         return ans;
-        
+//         return head;
 //     }
 // }
-
-class Solution {
-    public ListNode deleteDuplicates(ListNode head) {
-        ListNode curr = head;
-
-        while (curr != null && curr.next != null) {
-            if (curr.val == curr.next.val) {
-                // skip duplicate
-                curr.next = curr.next.next;
-            } else {
-                curr = curr.next;
-            }
-        }
-
-        return head;
-    }
-}
