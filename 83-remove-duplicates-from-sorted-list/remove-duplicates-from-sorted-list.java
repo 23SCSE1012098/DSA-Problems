58aa71084a271803150148e4c80efaa32eada5fd
Ram kumar <ramkumar724872@gmail.com>
2026-05-06 10:49:38 +0530

    Time: 0 ms (100.00%) | Memory: 45.5 MB (51.06%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.java
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.java
@@ -8,25 +8,43 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public ListNode deleteDuplicates(ListNode head) {
+//         if(head == null || head.next==null){
+//             return head;
+//         }
+//         ListNode dummy = new ListNode (head.val);
+//         ListNode ans = dummy;
+//         ListNode curr = dummy.next;
+//         while(curr != null){
+//             if(curr.val == dummy.val){
+//                 dummy.next = curr.next;
+//             }
+//             else{
+//                 dummy=dummy.next;
+//                 curr= curr.next;
+
+//             }
+            
+//         }
+//         return ans;
+        
+//     }
+// }
+
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        if(head==null || head.next==null){
-            return head;
-        }
-        ListNode dummy = new ListNode(head.val);
-        ListNode ans = dummy;
-        ListNode ptr = head.next;
-        while(ptr!=null){
-            if(ptr.val!=dummy.val){
-                ListNode temp = new ListNode(ptr.val);
-                dummy.next = temp;
-                dummy=dummy.next ;
+        ListNode curr = head;
 
-
+        while (curr != null && curr.next != null) {
+            if (curr.val == curr.next.val) {
+                // skip duplicate
+                curr.next = curr.next.next;
+            } else {
+                curr = curr.next;
             }
-            ptr=ptr.next;
         }
-        return ans;
-        
+
+        return head;
     }
 }
