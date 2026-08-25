03a7320376aa5b44eb8e028d32a9089261967188
Ram kumar <ramkumar724872@gmail.com>
2026-08-25 08:51:03 +0530

    Time: 826 ms (56.24%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1664-find-users-with-valid-e-mails/find-users-with-valid-e-mails.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+-- select user_id,name, mail from Users where mail REGEXP BINARY '^[A-Za-z][A-Za-z0-9_.-]*@leetcode[.]com$';
+
+SELECT user_id, name, mail
+FROM Users
+WHERE REGEXP_LIKE(
+    mail,
+    '^[A-Za-z][A-Za-z0-9_.-]*@leetcode[.]com$',
+    'c'
+);
