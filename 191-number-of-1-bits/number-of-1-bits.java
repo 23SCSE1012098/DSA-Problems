6499e858200cf4c2d5750f9da6a28aae6b5c8d4a
Ram kumar <ramkumar724872@gmail.com>
2026-09-05 14:32:07 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (95.68%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.java
+++ 191-number-of-1-bits/number-of-1-bits.java
@@ -12,14 +12,24 @@
 //     }
 // }
 
+// class Solution {
+//     public int hammingWeight(int n) {
+//         int count=0;
+//         while(n>0){
+//             count+=(n%2);
+//             n/=2;
+//         }
+//         return count;
+
+        
+//     }
+// }
+
+
+
 class Solution {
     public int hammingWeight(int n) {
-        int count=0;
-        while(n>0){
-            count+=(n%2);
-            n/=2;
-        }
-        return count;
+        return Integer.bitCount(n);
 
         
     }
